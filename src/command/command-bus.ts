a0335b6628a1e65be77935519665013cb3929660
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2021-09-20 15:33:02 +0200

    feat: new execute method arguments for MessageBus

--- src/command/command-bus.ts
+++ src/command/command-bus.ts
@@ -1,8 +1,9 @@
+import { Type } from '../index'
 import { Command } from './command'
 import { ICommandHandler } from './command-handler'
 
 export abstract class ICommandBus {
-  abstract execute<TCommand extends Command>(command: TCommand): Promise<any>
+  abstract execute<TCommand extends Command>(commandType: Type<TCommand>, ...args: any[]): Promise<any>
 
   abstract register(handlers: readonly ICommandHandler[]): void
 }
--- src/query/query-bus.ts
+++ src/query/query-bus.ts
@@ -1,8 +1,9 @@
+import { Type } from '../index'
 import { Query } from './query'
 import { IQueryHandler } from './query-handler'
 
 export abstract class IQueryBus {
-  abstract execute<TQuery extends Query, TRes>(query: TQuery): Promise<TRes>
+  abstract execute<TQuery extends Query, TRes>(queryType: Type<TQuery>, ...args: any[]): Promise<TRes>
 
   abstract register(handlers: ReadonlyArray<IQueryHandler>): void
 }
