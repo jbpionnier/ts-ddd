c3fb4d079bc59f84dd79152cd75559bd08acf85e
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2021-09-20 15:37:22 +0200

    feat: new execute method arguments for MessageHandler

--- src/message/index.ts
+++ src/message/index.ts
@@ -1,7 +1,5 @@
-import { Type } from '../index'
-
 export abstract class Message {}
 
 export interface MessageHandler<TMessage extends Message, TResponse = any> {
-  execute(messageType: Type<TMessage>, ...args: any[]): Promise<TResponse>
+  execute(message: TMessage): Promise<TResponse>
 }
